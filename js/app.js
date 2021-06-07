0614cde03fd1ed920114277e2589a4603d47c540
Walter Souto <wsouto@gmail.com>
2021-06-07 19:10:18 -0300

    Adds Lizard-Spock logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('Rock Paper Scissors Game is on!');
+console.log('Rock Paper Scissors Lizard Spock Game is on!');
 
 // Cache elements
 const scoreBoard_div = document.querySelector('.score-board');
@@ -10,12 +10,16 @@ const result_h1 = document.getElementById('result');
 const rock = document.getElementById('r');
 const paper = document.getElementById('p');
 const scissors = document.getElementById('s');
+const lizard = document.getElementById('l');
+const spock = document.getElementById('k');
 
 // Set hands
 let hands = {
   r: 'Rock',
   p: 'Paper',
   s: 'Scissors',
+  l: 'Lizard',
+  k: 'Spock',
 };
 
 // Initialize the scores
@@ -26,8 +30,8 @@ const smUser = '(user)'.fontcolor('green').fontsize(3).sub();
 const smComp = '(comp)'.fontcolor('orange').fontsize(3).sub();
 
 function getCompHand() {
-  const hands = ['r', 'p', 's'];
-  const choice = Math.floor(Math.random() * 3);
+  const hands = ['r', 'p', 's', 'l', 'k'];
+  const choice = Math.floor(Math.random() * 5);
 
   return hands[choice];
 }
@@ -39,7 +43,7 @@ function win(user, comp) {
   result_h1.innerHTML = `${hands[user]}${smUser} beats ${hands[comp]}${smComp}. You win!`;
 
   glow.classList.add('green-glow');
-  setTimeout(() => glow.classList.remove('green-glow'), 1000);
+  setTimeout(() => glow.classList.remove('green-glow'), 500);
 }
 
 function lose(user, comp) {
@@ -49,7 +53,7 @@ function lose(user, comp) {
   result_h1.innerHTML = `${hands[comp]}${smComp} beats ${hands[user]}${smUser}. You lose!`;
 
   glow.classList.add('red-glow');
-  setTimeout(() => glow.classList.remove('red-glow'), 1000);
+  setTimeout(() => glow.classList.remove('red-glow'), 500);
 }
 
 function draw(hand) {
@@ -58,26 +62,40 @@ function draw(hand) {
   result_h1.innerHTML = `${hands[hand]} equals ${hands[hand]}. It's a draw.`;
 
   glow.classList.add('gray-glow');
-  setTimeout(() => glow.classList.remove('gray-glow'), 1000);
+  setTimeout(() => glow.classList.remove('gray-glow'), 500);
 }
 
 function game(userHand) {
   const compHand = getCompHand();
 
   switch (userHand + compHand) {
-    case 'rs':
-    case 'pr':
     case 'sp':
+    case 'pr':
+    case 'rl':
+    case 'lk':
+    case 'ks':
+    case 'sl':
+    case 'pk':
+    case 'kr':
+    case 'rs':
       win(userHand, compHand);
       break;
-    case 'rp':
     case 'ps':
+    case 'rp':
+    case 'lr':
+    case 'lk':
+    case 'sk':
+    case 'ls':
+    case 'kp':
+    case 'rk':
     case 'sr':
       lose(userHand, compHand);
       break;
     case 'rr':
     case 'pp':
     case 'ss':
+    case 'll':
+    case 'kk':
       draw(userHand);
       break;
   }
@@ -88,6 +106,8 @@ function main() {
   rock.addEventListener('click', () => game('r'));
   paper.addEventListener('click', () => game('p'));
   scissors.addEventListener('click', () => game('s'));
+  lizard.addEventListener('click', () => game('l'));
+  spock.addEventListener('click', () => game('k'));
 }
 
 main();
