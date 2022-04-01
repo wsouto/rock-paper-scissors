e0e7eb6032928cdc20a8dcedf53de255829cd6bd
Luiselena Luna <lulues@gmail.com>
2022-04-01 19:48:22 -0300

    Inital conversion to typescript

---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Rock Paper Scissors Lizard Spock Game Module
+console.log("Rock Paper Scissors Lizard Spock Game is on!");
+// Cache elements
+var scoreBoard_div = document.querySelector(".score-board");
+var userScore_span = document.getElementById("user-score");
+var compScore_span = document.getElementById("comp-score");
+var result_h1 = document.getElementById("result");
+var rock = document.getElementById("r");
+var paper = document.getElementById("p");
+var scissors = document.getElementById("s");
+var lizard = document.getElementById("l");
+var spock = document.getElementById("k");
+// Set hands
+var hands = {
+    r: "Rock",
+    p: "Paper",
+    s: "Scissors",
+    l: "Lizard",
+    k: "Spock"
+};
+// Set action words
+var actions = {
+    sp: "cuts",
+    pr: "covers",
+    rl: "crushes",
+    lk: "poisons",
+    ks: "smashes",
+    sl: "decapitates",
+    lp: "eats",
+    pk: "disproves",
+    kr: "vaporizes",
+    rs: "crushes"
+};
+// Initialize the scores
+var userScore = 0;
+var compScore = 0;
+var smUser = "(user)".fontcolor("green").fontsize(3).sub();
+var smComp = "(comp)".fontcolor("orange").fontsize(3).sub();
+function getCompHand() {
+    var hands = ["r", "p", "s", "l", "k"];
+    var choice = Math.floor(Math.random() * 5);
+    return hands[choice];
+}
+function win(user, comp) {
+    var glow = document.getElementById(user);
+    var action = actions[user + comp];
+    userScore_span.innerText = (++userScore).toString();
+    result_h1.innerHTML = "".concat(hands[user]).concat(smUser, " ").concat(action, " ").concat(hands[comp]).concat(smComp, ". You win!");
+    glow.classList.add("green-glow");
+    setTimeout(function () { return glow.classList.remove("green-glow"); }, 500);
+}
+function lose(user, comp) {
+    var glow = document.getElementById(user);
+    var action = actions[comp + user];
+    compScore_span.innerText = (++compScore).toString();
+    result_h1.innerHTML = "".concat(hands[comp]).concat(smComp, " ").concat(action, " ").concat(hands[user]).concat(smUser, ". You lose!");
+    glow.classList.add("red-glow");
+    setTimeout(function () { return glow.classList.remove("red-glow"); }, 500);
+}
+function draw(hand) {
+    var glow = document.getElementById(hand);
+    result_h1.innerHTML = "".concat(hands[hand], " equals ").concat(hands[hand], ". It's a draw.");
+    glow.classList.add("gray-glow");
+    setTimeout(function () { return glow.classList.remove("gray-glow"); }, 500);
+}
+function game(userHand) {
+    var compHand = getCompHand();
+    switch (userHand + compHand) {
+        case "sp":
+        case "pr":
+        case "rl":
+        case "lk":
+        case "ks":
+        case "sl":
+        case "lp":
+        case "pk":
+        case "kr":
+        case "rs":
+            win(userHand, compHand);
+            break;
+        case "ps":
+        case "rp":
+        case "lr":
+        case "kl":
+        case "sk":
+        case "ls":
+        case "kp":
+        case "rk":
+        case "sr":
+            lose(userHand, compHand);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+        case "ll":
+        case "kk":
+            draw(userHand);
+            break;
+    }
+}
+// Main
+function main() {
+    rock.addEventListener("click", function () { return game("r"); });
+    paper.addEventListener("click", function () { return game("p"); });
+    scissors.addEventListener("click", function () { return game("s"); });
+    lizard.addEventListener("click", function () { return game("l"); });
+    spock.addEventListener("click", function () { return game("k"); });
+}
+main();
--- js/app.ts
+++ js/app.ts
@@ -1,122 +1,122 @@
 // Rock Paper Scissors Lizard Spock Game Module
 
-console.log('Rock Paper Scissors Lizard Spock Game is on!');
+console.log("Rock Paper Scissors Lizard Spock Game is on!");
 
 // Cache elements
-const scoreBoard_div = document.querySelector('.score-board');
-const userScore_span = document.getElementById('user-score');
-const compScore_span = document.getElementById('comp-score');
+const scoreBoard_div = document.querySelector(".score-board");
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
 
-const result_h1 = document.getElementById('result');
+const result_h1 = document.getElementById("result");
 
-const rock = document.getElementById('r');
-const paper = document.getElementById('p');
-const scissors = document.getElementById('s');
-const lizard = document.getElementById('l');
-const spock = document.getElementById('k');
+const rock = document.getElementById("r");
+const paper = document.getElementById("p");
+const scissors = document.getElementById("s");
+const lizard = document.getElementById("l");
+const spock = document.getElementById("k");
 
 // Set hands
 let hands = {
-  r: 'Rock',
-  p: 'Paper',
-  s: 'Scissors',
-  l: 'Lizard',
-  k: 'Spock',
+  r: "Rock",
+  p: "Paper",
+  s: "Scissors",
+  l: "Lizard",
+  k: "Spock",
 };
 
 // Set action words
 let actions = {
-  sp: 'cuts',
-  pr: 'covers',
-  rl: 'crushes',
-  lk: 'poisons',
-  ks: 'smashes',
-  sl: 'decapitates',
-  lp: 'eats',
-  pk: 'disproves',
-  kr: 'vaporizes',
-  rs: 'crushes',
+  sp: "cuts",
+  pr: "covers",
+  rl: "crushes",
+  lk: "poisons",
+  ks: "smashes",
+  sl: "decapitates",
+  lp: "eats",
+  pk: "disproves",
+  kr: "vaporizes",
+  rs: "crushes",
 };
 
 // Initialize the scores
 let userScore = 0;
 let compScore = 0;
 
-const smUser = '(user)'.fontcolor('green').fontsize(3).sub();
-const smComp = '(comp)'.fontcolor('orange').fontsize(3).sub();
+const smUser = "(user)".fontcolor("green").fontsize(3).sub();
+const smComp = "(comp)".fontcolor("orange").fontsize(3).sub();
 
 function getCompHand() {
-  const hands = ['r', 'p', 's', 'l', 'k'];
+  const hands = ["r", "p", "s", "l", "k"];
   const choice = Math.floor(Math.random() * 5);
 
   return hands[choice];
 }
 
-function win(user, comp) {
+function win(user: string, comp: string) {
   const glow = document.getElementById(user);
 
   let action = actions[user + comp];
 
-  userScore_span.innerText = ++userScore;
+  userScore_span.innerText = (++userScore).toString();
   result_h1.innerHTML = `${hands[user]}${smUser} ${action} ${hands[comp]}${smComp}. You win!`;
 
-  glow.classList.add('green-glow');
-  setTimeout(() => glow.classList.remove('green-glow'), 500);
+  glow.classList.add("green-glow");
+  setTimeout(() => glow.classList.remove("green-glow"), 500);
 }
 
-function lose(user, comp) {
+function lose(user: string, comp: string) {
   const glow = document.getElementById(user);
 
   let action = actions[comp + user];
 
-  compScore_span.innerText = ++compScore;
+  compScore_span.innerText = (++compScore).toString();
   result_h1.innerHTML = `${hands[comp]}${smComp} ${action} ${hands[user]}${smUser}. You lose!`;
 
-  glow.classList.add('red-glow');
-  setTimeout(() => glow.classList.remove('red-glow'), 500);
+  glow.classList.add("red-glow");
+  setTimeout(() => glow.classList.remove("red-glow"), 500);
 }
 
-function draw(hand) {
+function draw(hand: string) {
   const glow = document.getElementById(hand);
 
   result_h1.innerHTML = `${hands[hand]} equals ${hands[hand]}. It's a draw.`;
 
-  glow.classList.add('gray-glow');
-  setTimeout(() => glow.classList.remove('gray-glow'), 500);
+  glow.classList.add("gray-glow");
+  setTimeout(() => glow.classList.remove("gray-glow"), 500);
 }
 
-function game(userHand) {
+function game(userHand: string) {
   const compHand = getCompHand();
 
   switch (userHand + compHand) {
-    case 'sp':
-    case 'pr':
-    case 'rl':
-    case 'lk':
-    case 'ks':
-    case 'sl':
-    case 'lp':
-    case 'pk':
-    case 'kr':
-    case 'rs':
+    case "sp":
+    case "pr":
+    case "rl":
+    case "lk":
+    case "ks":
+    case "sl":
+    case "lp":
+    case "pk":
+    case "kr":
+    case "rs":
       win(userHand, compHand);
       break;
-    case 'ps':
-    case 'rp':
-    case 'lr':
-    case 'kl':
-    case 'sk':
-    case 'ls':
-    case 'kp':
-    case 'rk':
-    case 'sr':
+    case "ps":
+    case "rp":
+    case "lr":
+    case "kl":
+    case "sk":
+    case "ls":
+    case "kp":
+    case "rk":
+    case "sr":
       lose(userHand, compHand);
       break;
-    case 'rr':
-    case 'pp':
-    case 'ss':
-    case 'll':
-    case 'kk':
+    case "rr":
+    case "pp":
+    case "ss":
+    case "ll":
+    case "kk":
       draw(userHand);
       break;
   }
@@ -124,11 +124,11 @@ function game(userHand) {
 
 // Main
 function main() {
-  rock.addEventListener('click', () => game('r'));
-  paper.addEventListener('click', () => game('p'));
-  scissors.addEventListener('click', () => game('s'));
-  lizard.addEventListener('click', () => game('l'));
-  spock.addEventListener('click', () => game('k'));
+  rock.addEventListener("click", () => game("r"));
+  paper.addEventListener("click", () => game("p"));
+  scissors.addEventListener("click", () => game("s"));
+  lizard.addEventListener("click", () => game("l"));
+  spock.addEventListener("click", () => game("k"));
 }
 
 main();
